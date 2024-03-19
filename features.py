bd4898b08eee866e68b58c3f39d09e3fafd0ab00
Ekaterina <Katya.Kornil10@gmail.com>
2024-03-19 21:57:28 +0300

    change files

--- features.py
+++ features.py
@@ -4,16 +4,24 @@ import os
 from warnings import simplefilter
 simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
+root = 'Profile-task-Data-Scientist/data'
 
-def init():
-    root = 'data'
+
+def init_features():
     features = pd.read_csv(os.path.join(root, 'features.csv'))
     train = pd.read_csv(os.path.join(root, 'train.csv'))
     test = pd.read_csv(os.path.join(root, 'test.csv'))
-    prepare_df(train, test, features)
+
+    feat_cols = features.columns.values[2:-2]
+    train, test, features = prepare_df(train, test, features, feat_cols)
+
+    train = fill_df(features, train, feat_cols)
+    test = fill_df(features, test, feat_cols)
+
+    save_new_data(train, test)
 
 
-def prepare_df(train, test, features):
+def prepare_df(train, test, features, feat_cols):
     dfs = [train, test, features]
 
     for l in ['lat', 'lon']:
@@ -26,58 +34,41 @@ def prepare_df(train, test, features):
     features["id"] = list(range(0, features.shape[0]))
     
 
-
-def 
-    feat_cols = features.columns.values[2:-2]
-    coords_f = features.coord.values
-    coords_tr = train.coord.values
-    coords_ts = test.coord.values
-
     for f in feat_cols:
         train[f] = np.zeros(train.shape[0])
         test[f] = np.zeros(test.shape[0])
 
-def fill_df(features, df):
+    return train, test, features
+
+
+def fill_df(features, df, feat_cols):
+    all_js = set()
+    coords_f = features.coord.values
+    coords = df.coord.values
 
     for i in range(len(coords_f)):
         f_i = coords_f[i]
 
-        if (f_i in coords_tr) or ((f_i+0.1) in coords_tr) or ((f_i-0.1) in coords_tr):
-            if f_i in coords_tr: js = np.where (coords_tr == f_i)
-            elif (f_i+0.1) in coords_tr: js = np.where (coords_tr == (f_i+0.1))
-            elif (f_i-0.1) in coords_tr: js = np.where (coords_tr == (f_i-0.1))
+        if (f_i in coords) or ((f_i+0.1) in coords) or ((f_i-0.1) in coords):
+            if f_i in coords: js = np.where (coords == f_i)
+            elif (f_i+0.1) in coords: js = np.where (coords == (f_i+0.1))
+            elif (f_i-0.1) in coords: js = np.where (coords == (f_i-0.1))
 
-            for j in js:
-                for id, f in enumerate(features.loc[i][2:-2].values):
-                    train.loc[j, str(id)] = f
+            for j in js[0]:
+                if j not in all_js:
+                    for id, f in enumerate(features.loc[i][2:-2].values):
+                        df.loc[j, str(id)] = f
+                    all_js.add(j)
 
-    for i in range(train.shape[0]):
-        if df .loc[i, feat_cols].sum() == 0:
-            df = train.drop(i)
+    for i in range(df.shape[0]):
+        if df.loc[i, feat_cols].sum() == 0:
+            df = df.drop(i)
+    
+    return df
 
 
-train.drop(["id"], axis=1).to_csv(os.path.join(root, "train_features.csv"))
-pd.DataFrame({"id": train["id"]}).to_csv(os.path.join(root, "train_ids.csv"))
+def save_new_data(train, test):
+    train.drop(["id"], axis=1).to_csv(os.path.join(root, "train_features.csv"))
 
-
-for i in ranage(len(coords_f)):
-  f_i = coords_f[i]
-
-  if (f_i in coords_ts) or ((f_i+0.1) in coords_ts) or ((f_i-0.1) in coords_ts):
-    if f_i in coords_ts: js = np.where (coords_ts == f_i)
-    elif (f_i+0.1) in coords_ts: js = np.where (coords_ts == (f_i+0.1))
-    elif (f_i-0.1) in coords_ts: js = np.where (coords_ts == (f_i-0.1))
-
-    for j in js:
-      for id, f in enumerate(features.loc[i][2:-2].values):
-        test.loc[j, str(id)] = f
-
-for i in range(test.shape[0]):
-  if test.loc[i, feat_cols].sum() == 0:
-    test = test.drop(i)
-
-test.drop(["id"], axis=1).to_csv(os.path.join(root, "test_features.csv"))
-pd.DataFrame({"id": test["id"]}).to_csv(os.path.join(root, "test_ids.csv"))
-
-if __name__ == '__main__':
-	init()
+    test.drop(["id"], axis=1).to_csv(os.path.join(root, "test_features.csv"))
+    pd.DataFrame({"id": test["id"]}).to_csv(os.path.join(root, "test_ids.csv"))
--- train.py
+++ train.py
@@ -1,42 +1,55 @@
-
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import catboost as cb
+
+import lightgbm as lgb
+
+from features import init_features
+
+data_root = 'Profile-task-Data-Scientist/data'
+model_root = 'Profile-task-Data-Scientist/models'
 
 
-data_root = 'data'
-model_root = 'model'
+def init():
+    init_features()
 
-train_features = pd.read_csv(os.path.join(data_root, 'train_features.csv'))
-test_features = pd.read_csv(os.path.join(data_root, 'test_features.csv'))
-test_ids = pd.read_csv(os.path.join(data_root, 'test_ids.csv'))
+    train_features = pd.read_csv(os.path.join(data_root, 'train_features.csv'))
+    test_features = pd.read_csv(os.path.join(data_root, 'test_features.csv'))
+    test_ids = pd.read_csv(os.path.join(data_root, 'test_ids.csv'))
+    
+    X, y = train_features.drop(["score", "Unnamed: 0"], axis=1), train_features["score"]
+    X_train, X_val, y_train,  y_val = train_test_split(X, y, random_state=42, test_size=0.2)
+    X_test, test_ids = test_features.drop(["Unnamed: 0"], axis=1), test_ids.drop(["Unnamed: 0"], axis=1)
+
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+
+    X_train = scaler.transform(X_train)
+    X_val = scaler.transform(X_val)
+    X_test = scaler.transform(X_test)
+
+    train_lgb_reg(X_train, X_val, y_train,  y_val)
+    predict(X_test, test_ids)
 
 
-X, y = train_features.drop(["score", "Unnamed: 0"], axis=1), train_features["score"]
-X_train, X_val, y_train,  y_val = train_test_split(X, y, random_state=42, test_size=0.2)
+def train_lgb_reg(X_train, X_val, y_train,  y_val):
+    model_lgb = LGBMRegressor(metric='rmse', random_state=42)
+    model_lgb.fit(X_train, y_train)
+    pred_lgbreg = model_lgb.predict(X_val)
+    rmse = np.sqrt(mean_squared_error(y_val, pred_lgbreg))
+    
+    model_lgb.booster_.save_model(os.path.join(model_root, 'lgbm_reg.txt'))
+    
+    rmse = train_lgb_reg(X_train, X_val, y_train,  y_val)
 
-X_test, test_ids = test_features.drop(["Unnamed: 0"], axis=1), test_ids.drop(["Unnamed: 0"], axis=1)
+    with open(os.path.join(data_root, "rmse.txt")) as f:
+            f.write(str(rmse))    
+    
+
+def predict(X_test, test_ids):
+    model_lgb = lgb.Booster(model_file=os.path.join(model_root, 'lgbm_reg.txt'))
+    preds = model_lgb.predict(X_test)
+    pd.DataFrame({'id': test_ids.id, 'score': preds}).to_csv(os.path.join(data_root, "submission.csv"))
 
 
-params = {'learning_rate': 0.008,
-          'depth': 8,
-          'subsample': 0.3,
-          'colsample_bylevel': 0.82,
-          'min_data_in_leaf': 15,
-          "iterations": 1000,
-          "random_state": 42,
-          "eval_metric": "RMSE"
-          }
-
-model = cb.CatBoostRegressor(**params)
-model.fit(X_train, y_train, eval_set=(X_val, y_val), use_best_model=True)
-model.save_model(os.path.join(model_root, "catboost.cbm"), format="cbm")
-
-predictions = model.predict(X_val)
-rmse = mean_squared_error(y_val, predictions, squared=False)
-
-with open(os.path.join(data_root, "preiction_score.txt")) as f:
-    f.write(str(rmse))
-
-preds = model.predict(X_test)
-pd.DataFrame({'id': test_ids.id, 'score': preds}).to_csv(os.path.join(data_root, "submission.csv"))
+if __name__ == '__main__':
+	init()
